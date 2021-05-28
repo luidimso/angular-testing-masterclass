b27749690780ad19d24540c716d0f96e4e77d9ad
luidimso <luidi_mso@outlook.com>
2021-05-28 15:36:31 -0300

    Testing component creation

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {CoursesCardListComponent} from './courses-card-list.component';
 import {CoursesModule} from '../courses.module';
 import {COURSES} from '../../../../server/db-data';
@@ -9,17 +9,23 @@ import {Course} from '../model/course';
 import {setupCourses} from '../common/setup-test-data';
 
 describe('CoursesCardListComponent', () => {
-  beforeEach(() => {
+  let component:CoursesCardListComponent;
+  let fixture:ComponentFixture<CoursesCardListComponent>;
+
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         CoursesModule
       ]
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(CoursesCardListComponent);
+      component = fixture.componentInstance; 
     });
-  });
+  }));
 
 
   it("should create the component", () => {
-   pending();
+    expect(component).toBeTruthy();
   });
 
 
