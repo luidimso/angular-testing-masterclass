4b00949f295511bdcb50126da98a93da16cd25c8
luidimso <luidi_mso@outlook.com>
2021-05-28 18:38:15 -0300

    Testing anothers home tabs cases

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -22,6 +22,8 @@ describe('HomeComponent', () => {
   let coursesService:any;
 
   const beginnerCourses = setupCourses().filter(course => course.category == 'BEGINNER');
+  const advancedCourses = setupCourses().filter(course => course.category == 'ADVANCED');
+
   const coursesServiceSpy = jasmine.createSpyObj('CoursesService', ['findAllCourses']);
 
   beforeEach(waitForAsync(() => {
@@ -59,12 +61,18 @@ describe('HomeComponent', () => {
 
 
   it("should display only advanced courses", () => {
-    pending();
+    coursesService.findAllCourses.and.returnValue(of(advancedCourses));
+    fixture.detectChanges();
+    const tabs = element.queryAll(By.css(".mat-tab-label"));
+    expect(tabs.length).toBe(1, "Unexpected number of tabs found");
   });
 
 
   it("should display both tabs", () => {
-    pending();
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+    fixture.detectChanges();
+    const tabs = element.queryAll(By.css(".mat-tab-label"));
+    expect(tabs.length).toBe(2, "Unexpected number of tabs found");
   });
 
 
