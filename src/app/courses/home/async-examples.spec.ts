01aae603e0faea492456d319315318f919ef487a
luidimso <luidi_mso@outlook.com>
2021-05-31 14:09:21 -0300

    Simple example with asyncronous test

---
+++ src/app/courses/home/async-examples.spec.ts
@@ -0,0 +1,10 @@
+describe("Async Testing Examples", () => {
+    it("Asynchronous test example with Jasmine done()", (done:DoneFn) => {
+        let test = false;
+        setTimeout(() => {
+            test = true;
+            expect(test).toBeTruthy();
+            done();
+        });
+    }, 1000);
+});
